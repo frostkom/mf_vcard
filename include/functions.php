110c88606148307f611ec10c1e2b125f3c62d274
frostkom <martin.fors@frostkom.se>
2015-09-15 12:10:19 +0200

    Add recommendation of MF Form to be installed

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+function admin_init_vcard()
+{
+	new recommend_plugin("mf_form/index.php", "MF Form", "//github.com/frostkom/mf_form");
+}
+
 function widgets_vcard()
 {
 	register_widget('widget_vcard');
--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 /*
 Plugin Name: MF vCard
 Plugin URI: www.github.com/frostkom/mf_vcard
-Version: 1.0.2
+Version: 1.0.3
 Author: Martin Fors
 Author URI: www.frostkom.se
 */
 
+add_action('admin_init', 'admin_init_vcard');
 add_action('widgets_init', 'widgets_vcard');
 
 load_plugin_textdomain('lang_vcard', false, dirname(plugin_basename(__FILE__)).'/lang/');
