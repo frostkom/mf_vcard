282109444de4642ccbac96a88367972c7e4cfe80
frostkom <martin.fors@frostkom.se>
2024-04-23 11:09:49 +0200

    Add new icon and remove readme

--- README.md
+++
@@ -1,9 +0,0 @@
-## Description
-Wordpress plugin to add vCard widgets.
-
-## Installation
-1. Upload the folder to /wp-content/plugins/
-2. Activate the plugin through Plugins
-
-## Dependencies
-MF Base
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF vCard
 Plugin URI: https://github.com/frostkom/mf_vcard
 Description:
-Version: 2.5.7
+Version: 2.5.8
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
