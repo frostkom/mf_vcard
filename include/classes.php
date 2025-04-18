cfa2bb85b9504a4c75399ec21edc3de823db63f0
Martin Fors <martin.fors@frostkom.se>
2025-04-18 12:10:26 +0200

    Move block theme check to when widgets are about to be initiated

--- include/classes.php
+++ include/classes.php
@@ -323,7 +323,10 @@ class mf_vcard
 
 	function widgets_init()
 	{
-		register_widget('widget_vcard');
+		if(wp_is_block_theme() == false)
+		{
+			register_widget('widget_vcard');
+		}
 	}
 }
 
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF vCard
 Plugin URI: https://github.com/frostkom/mf_vcard
 Description:
-Version: 2.6.12
+Version: 2.6.13
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
@@ -34,10 +34,7 @@ if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') &
 		add_action('wp_head', array($obj_vcard, 'wp_head'), 0);
 	}
 
-	if(wp_is_block_theme() == false)
-	{
-		add_action('widgets_init', array($obj_vcard, 'widgets_init'));
-	}
+	add_action('widgets_init', array($obj_vcard, 'widgets_init'));
 
 	function uninstall_vcard()
 	{
